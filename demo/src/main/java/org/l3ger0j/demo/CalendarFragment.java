d333929731915b8d5418571f6fe97a147aa4f0a7
l3ger0j <83360444+l3ger0j@users.noreply.github.com>
2022-04-12 22:23:26 +0300

    Update to 1.1.4 Remove NinePath file and png file. Now image get in Vector And remove old calendar generator. Now calendarView

--- demo/src/main/java/org/l3ger0j/demo/CalendarFragment.java
+++ demo/src/main/java/org/l3ger0j/demo/CalendarFragment.java
@@ -10,50 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarFragment extends Fragment {
 
-    private ListView listView;
+    private CalendarView calendarView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View parentView = inflater.inflate(R.layout.calendar , container , false);
-        listView = parentView.findViewById(R.id.listView);
+        calendarView = parentView.findViewById(R.id.calendarView);
         initView();
         return parentView;
     }
 
     private void initView(){
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                getCalendarData());
-        listView.setAdapter(arrayAdapter);
-        listView.setOnItemClickListener((adapterView , view , i , l) ->
+        calendarView.setOnDateChangeListener((view , year , month , dayOfMonth) ->
                 Toast.makeText(getActivity(), "Clicked item!", Toast.LENGTH_LONG).show());
-    }
-
-    @NonNull
-    private ArrayList<String> getCalendarData(){
-        ArrayList<String> calendarList = new ArrayList<>();
-        calendarList.add("New Year's Day");
-        calendarList.add("St. Valentine's Day");
-        calendarList.add("Easter Day");
-        calendarList.add("April Fool's Day");
-        calendarList.add("Mother's Day");
-        calendarList.add("Memorial Day");
-        calendarList.add("National Flag Day");
-        calendarList.add("Father's Day");
-        calendarList.add("Independence Day");
-        calendarList.add("Labor Day");
-        calendarList.add("Columbus Day");
-        calendarList.add("Halloween");
-        calendarList.add("All Soul's Day");
-        calendarList.add("Veterans Day");
-        calendarList.add("Thanksgiving Day");
-        calendarList.add("Election Day");
-        calendarList.add("Forefather's Day");
-        calendarList.add("Christmas Day");
-        return calendarList;
     }
 }
